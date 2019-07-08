dfb262c2d01a58c1c10d1287ee22983cbdc44221
Jayesh Lathiya <jayjayesh@users.noreply.github.com>
2019-07-08 18:38:31 +0530

    1st commit  = parse json like pro

---
+++ day_01_02_parse_json_like_pro/android/app/src/main/java/com/example/day_01_02_parse_json_like_pro/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.day_01_02_parse_json_like_pro;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
