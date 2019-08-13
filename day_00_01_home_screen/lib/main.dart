267c2e35f6b9292a38281dcf080cdbf3796c6d54
Jayesh Lathiya <jayjayesh@users.noreply.github.com>
2019-08-13 10:53:10 +0530

    basic

--- day_00_01_home_screen/lib/main.dart
+++ day_00_01_home_screen/lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:day_00_01_home_screen/Screens/home_screen.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+//void main() => runApp(MyApp());
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+Future<void> main() async {
+//StatusBar change color
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+//Application Fixed in  (portrait) mode only
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+// Do any initialisation: Shared_prefrence, Singleton, etc
+
   runApp(MyApp());
 }
 
