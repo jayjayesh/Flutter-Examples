49bdfafcc09da5185b9da3e069f735102e8fc643
Jayesh Lathiya <jayjayesh@users.noreply.github.com>
2019-08-13 12:34:06 +0530

    imlemented  assets images and font

--- day_00_01_home_screen/lib/Screens/home_screen.dart
+++ day_00_01_home_screen/lib/Screens/home_screen.dart
@@ -8,7 +8,47 @@ class HomeScreen extends StatelessWidget {
         title: Text('Home Screen'),
       ),
       body: Container(
-        color: Colors.grey,
+        height: double.infinity,
+        width: double.infinity,
+        color: Colors.yellow[100],
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text('Hi Check'),
+              Text(
+                'Text Custom font family',
+                style: TextStyle(
+                  fontFamily: 'google-sans-cufonfonts',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
+              ),
+              Image.asset(
+                'assets/images/girl.jpg',
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Image.asset(
+                'assets/images/bird.jpg',
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Image.asset(
+                'assets/images/flower.jpg',
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+              ),
+            ]),
       ),
     );
   }
---
+++ day_00_01_home_screen/lib/flutter_example_projects.dart
@@ -0,0 +1,28 @@
+/**
+ 
+//TODO: day_00_01_home_screen
+//?  Basic Library (Cocoa pods)
+//?  Load assets images
+//?  Load custom fonts
+
+
+//TODO: day_03_01_left_menu_properway_pageview
+//?  Pageview, LeftMenu
+
+
+
+//TODO: day_07_04_ui_textfield_predict_gender
+//?  Textfield, Check-Internet conectivity, 
+//?  CallWebService (get) parse JSON-to-Map(Dictionary), 
+//?  FIX:SneakBar does not show inside onButtonPress method, 
+//?  FIX: textfield Hide Keyboard
+
+
+
+//TODO: day_07_05_ui_simple_login_screen
+//?  Textfield next focus, 
+//?  Email validation, Secure Text, 
+//?  FIX:SneakBar does not show Solved by (new Build) widget and Creating object of Scafold_Context, 
+
+
+*/
