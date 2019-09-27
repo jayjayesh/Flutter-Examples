fc241f16c3a6923f705af6c6e788bfa4293533b8
Jayesh Lathiya <jayjayesh@users.noreply.github.com>
2019-09-27 19:12:03 +0530

    Setup Top_TabBar

--- day_07_08_ui_tab_bar_setup/lib/main.dart
+++ day_07_08_ui_tab_bar_setup/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:day_07_08_ui_tab_bar_setup/homeScreen.dart';
+import 'package:day_07_08_ui_tab_bar_setup/homeScreen_TopTabBar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,10 +13,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         //primarySwatch: Colors.red,
         primaryColor: const Color(0xFFF44336), // Colors.red,
-        accentColor: const Color(0xFF2196F3), // Colors.blue,
+        accentColor: const Color(0xFFFFFB3B), // Colors.yellow,
         primaryColorBrightness: Brightness.dark,
       ),
-      home: HomeScreen(),
+      //home: HomeScreen(),
+      home: HomeScreenTopTabBar(),
     );
   }
 }
