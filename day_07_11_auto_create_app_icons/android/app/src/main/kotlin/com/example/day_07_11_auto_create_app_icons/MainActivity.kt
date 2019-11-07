63ef781355db430c67ae9345df410659dbbe99b2
Jayesh Lathiya <jayjayesh@users.noreply.github.com>
2019-11-07 19:15:06 +0530

    Learn How to add android & ios specific app icons

---
+++ day_07_11_auto_create_app_icons/android/app/src/main/kotlin/com/example/day_07_11_auto_create_app_icons/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.day_07_11_auto_create_app_icons
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
