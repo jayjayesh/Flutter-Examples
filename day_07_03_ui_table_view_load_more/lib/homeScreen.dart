2caf82d32f61cba48ad6974c074d10547d937193
Jayesh Lathiya <jayjayesh@users.noreply.github.com>
2019-11-17 20:07:06 +0530

    Added Pull to refresh

--- day_07_03_ui_table_view_load_more/lib/homeScreen.dart
+++ day_07_03_ui_table_view_load_more/lib/homeScreen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -12,6 +15,8 @@ class _HomeScreenClassState extends State<HomeScreenClass> {
   int _nMaxCount = 100;
   int _nCurrentMax = 0;
   ScrollController _scrollControllerObject = ScrollController();
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      new GlobalKey<RefreshIndicatorState>();
 
   @override
   void initState() {
@@ -28,7 +33,9 @@ class _HomeScreenClassState extends State<HomeScreenClass> {
     _scrollControllerObject.addListener(() {
       if (_scrollControllerObject.position.pixels ==
           _scrollControllerObject.position.maxScrollExtent) {
-        _getMoreItems();
+        Timer(const Duration(seconds: 2), () {
+          _getMoreItems();
+        });
       }
     });
   }
@@ -47,39 +54,94 @@ class _HomeScreenClassState extends State<HomeScreenClass> {
     });
   }
 
+  Future<Null> _refreshTableviewGetNewArticles() async {
+    return Future.delayed(const Duration(seconds: 2), () {
+      arrTableViewItems.insert(0, 'item : ${DateTime.now()}');
+      setState(() {
+        // Here you can write your code for open new view
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
+        actions: <Widget>[
+          new IconButton(
+              icon: const Icon(Icons.refresh),
+              tooltip: 'Refresh',
+              onPressed: () {
+                _refreshIndicatorKey.currentState.show();
+              }),
+        ],
       ),
       body: Container(
-        child: ListView.builder(
-          controller: _scrollControllerObject,
-          itemExtent: 80.0,
-          itemCount: arrTableViewItems.length + 1,
-          itemBuilder: (BuildContext context, int index) {
-            print(
-                '\n itemBuilder index == arrTableViewItems.length  ($index == ${arrTableViewItems.length})');
-            //At Last Cell
-            if (index == arrTableViewItems.length) {
-              if (index < _nMaxCount) {
-                return CupertinoActivityIndicator();
-              } else {
-                //if all data loaded, don't shoe activity indicator
-                return SizedBox(
-                  height: 0.0,
-                );
+        child: RefreshIndicator(
+          key: _refreshIndicatorKey,
+          onRefresh: _refreshTableviewGetNewArticles,
+          child: ListView.builder(
+            controller: _scrollControllerObject,
+            itemExtent: 80.0,
+            itemCount: arrTableViewItems.length + 1,
+            itemBuilder: (BuildContext context, int index) {
+              print(
+                  '\n itemBuilder index == arrTableViewItems.length  ($index == ${arrTableViewItems.length})');
+              //At Last Cell
+              if (index == arrTableViewItems.length) {
+                if (index < _nMaxCount) {
+                  return CupertinoActivityIndicator();
+                } else {
+                  //if all data loaded, don't shoe activity indicator
+                  return SizedBox(
+                    height: 0.0,
+                  );
+                }
               }
-            }
 
-            //All Normal Cell
-            return ListTile(
-              title: Text(arrTableViewItems[index]),
-            );
-          },
+              //All Normal Cell
+              return ListTile(
+                title: Text(arrTableViewItems[index]),
+              );
+            },
+          ),
         ),
       ),
     );
   }
 }
+
+/*
+// (1) Repeat code every 2-second 
+// (2) Stop Timer 
+// (3) Delay Method Calling
+
+
+import 'dart:async';
+
+void fetchDataEvery2Second() {
+  
+  var index = 0;
+  Timer.periodic(const Duration(seconds: 2), (Timer timeeer){
+    print("Flutter $index");
+   index++;
+   if(index == 11)
+   {
+     timeeer.cancel();
+   }
+  });
+}
+
+
+//--------------------------------
+
+import 'dart:async';
+
+ Timer(const Duration(seconds: 20), (){
+    print("Delay Method Calling = Flutter One Shot = Does not repeat");
+  });
+
+//--------------------------------
+
+*/
