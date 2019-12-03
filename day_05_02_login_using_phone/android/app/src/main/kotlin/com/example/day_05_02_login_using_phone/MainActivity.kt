f5642a281a996b5a35bb798db600214745091327
Jayesh Lathiya <jayjayesh@users.noreply.github.com>
2019-12-03 20:02:12 +0530

    initial commit

---
+++ day_05_02_login_using_phone/android/app/src/main/kotlin/com/example/day_05_02_login_using_phone/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.day_05_02_login_using_phone
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ day_05_02_login_using_phone/lib/appDelegate.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class appDelegate extends StatefulWidget {
+  @override
+  _appDelegateState createState() => _appDelegateState();
+}
+
+class _appDelegateState extends State<appDelegate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
