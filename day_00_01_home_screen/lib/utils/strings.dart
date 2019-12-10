04094b04362aa1a84f86b4c81eb6bca1862e915c
Jayesh Lathiya <jayjayesh@users.noreply.github.com>
2019-12-10 20:16:47 +0530

    Clean Some Code

--- day_00_01_home_screen/lib/utils/strings.dart
+++
@@ -1,6 +0,0 @@
-//! Type all strings here, It will Help when this app will be Localized in Future. (Local Language)
-
-class AllStrings {
-  static String strHi = 'Hi';
-  static String strHomeScreen = 'Home Screen';
-}
