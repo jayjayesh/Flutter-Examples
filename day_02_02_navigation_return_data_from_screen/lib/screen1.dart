086a4f1c0edc1191cf9672a96b01f0447c77007d
Jayesh Lathiya <jayjayesh@users.noreply.github.com>
2019-12-25 23:11:14 +0530

    Update screen1.dart

--- day_02_02_navigation_return_data_from_screen/lib/screen1.dart
+++ day_02_02_navigation_return_data_from_screen/lib/screen1.dart
@@ -34,14 +34,18 @@ class Screen1 extends StatelessWidget {
   }
 
   _navigateAndDisplaySelectionData(BuildContext context) async {
-    final selectedData = await Navigator.pushNamed(context, Screen2.routeName);
-    print('$selectedData');
-    //Scaffold.of(context).removeCurrentSnackBar();
-    //Scaffold.of(context).showSnackBar(SnackBar(
-    //   content: Text('$selectedData'),
-    //));
+    final var selectedData = await Navigator.pushNamed(context, Screen2.routeName);
+    
+    if(selectedData != null)
+    {
+      print('$selectedData');
+      //Scaffold.of(context).removeCurrentSnackBar();
+      //Scaffold.of(context).showSnackBar(SnackBar(
+      //   content: Text('$selectedData'),
+      //));
 
-    showAlertDialog(context, 'My Title', 'You Selected : ($selectedData' + ')');
+      showAlertDialog(context, 'My Title', 'You Selected : ($selectedData' + ')');
+    }
   }
 
   @override
