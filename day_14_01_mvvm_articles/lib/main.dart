833b0e65f5bde5363fe5c8ae205910a1a4f0be06
Jayesh Lathiya <jayjayesh@users.noreply.github.com>
2020-01-19 00:34:42 +0530

    .

--- day_14_01_mvvm_articles/lib/main.dart
+++ day_14_01_mvvm_articles/lib/main.dart
@@ -11,6 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       home: ChangeNotifierProvider(
         create: (context) => ArticleListViewModel(),
         child: HomePage(),
